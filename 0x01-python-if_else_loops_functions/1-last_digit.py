1254b26ab422159b68ddfec9aa4be10286f0ef35
BMWire <bmwemawire@gmail.com>
2022-05-31 13:31:48 +0300

    chore: create 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+# get the last digit of number when it is signed
+last_digit = abs(number) % 10
+
+if (last_digit > 5):
+    print("Last digit of {} is {} and is greater than 5\n".format(number, last_digit))
+elif (last_digit == 0):
+    print("Last digit of {} is {} and is 0\n".format(number, last_digit))
+elif (last_digit > 6 and last_digit != 0):
+    print("Last digit of {} is {} and is less than 6 and not 0\n".format(number, last_digit))
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -3,3 +3,4 @@
 This directory contains the following files:
 
 1. 0-positive_or_negative.py - Checks to see if the random number generated each time is positive or negative.
+2. 1-last_digit.phy - Get the last digit of a random number then check to see whether it is greater than or less than five and 0.
