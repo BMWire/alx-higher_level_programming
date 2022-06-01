0afc23c27e8177c6e57a7ad6e43095b27d542932
BMWire <bmwemawire@gmail.com>
2022-06-01 20:33:27 +0300

    fix: pycodesytle reinforcement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,15 @@ import random
 number = random.randint(-10000, 10000)
 
 # get the last digit of number when it is signed
-last_digit = abs(number) % 10
+if number < 0:
+    last_digit = number % 10
+else:
+    last_digit = abs(number) % 10
 
+print('Last digit of', number, 'is', last_digit, end='')
 if (last_digit > 5):
-    print("Last digit of {} is {} and is greater than 5\n".format(number, last_digit))
+    print('and is greater than 5\n')
 elif (last_digit == 0):
-    print("Last digit of {} is {} and is 0\n".format(number, last_digit))
-elif (last_digit > 6 and last_digit != 0):
-    print("Last digit of {} is {} and is less than 6 and not 0\n".format(number, last_digit))
+    print('and is 0\n')
+else:
+    print('and is less than 6 and not 0\n')
