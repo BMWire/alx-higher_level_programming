b569e68e68903d4ca4bfa07e7caaabafd5d39ad7
BMWire <bmwemawire@gmail.com>
2022-06-08 18:22:12 +0300

    fix:9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    # return a new dictionary whose values are 2* the value of initial dictionary
+    # return a new dictionary whose values are 2 times
+    # the value of initial dictionary
     return {key: value * 2 for key, value in a_dictionary.items()}
