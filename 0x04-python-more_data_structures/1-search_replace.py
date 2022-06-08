a8f73ae42b04f7f55753d3e4cde64ad20fb687fd
BMWire <bmwemawire@gmail.com>
2022-06-08 16:43:41 +0300

    fix: python version; 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python2
 
 def search_replace(my_list, search, replace):
-    # search
     for index, search in enumerate(my_list):
         if search:
             my_list[index] = replace
