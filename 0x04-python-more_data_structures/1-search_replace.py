df133340b45474b72189b9c4b9f89cbd2870c145
BMWire <bmwemawire@gmail.com>
2022-06-08 17:14:28 +0300

    fix:  1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,4 +4,6 @@ def search_replace(my_list, search, replace):
     for index, search in enumerate(my_list):
         if search:
             my_list[index] = replace
+        else: 
+            my_list[index] = search
     return my_list
