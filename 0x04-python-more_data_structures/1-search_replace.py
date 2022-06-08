6f80074f71b10e66c2e74736f67aa3da48b809bf
BMWire <bmwemawire@gmail.com>
2022-06-08 17:19:59 +0300

    fix:  1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    for index, search in enumerate(my_list):
-        if search:
-            my_list[index] = replace
-        else: 
-            my_list[index] = search
-    return my_list
+    def func(element):
+        return (element if element != search else replace)
+    return list(map(func, my_list))
